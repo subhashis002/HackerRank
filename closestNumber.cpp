2e6cdb8bd975cbf3fb56c681606777f852890cef
subhashis002 <subhashis002@gmail.com>
2020-03-16 07:45:39 +0000

    first commit

---
+++ closestNumber.cpp
@@ -0,0 +1,160 @@
+/*
+ * Sorting is useful as the first step in many different tasks. The most common task is to make finding things easier, but there are other uses as well. In this case, it will make it easier to determine which pair or pairs of elements have the smallest absolute difference between them.
+ *
+ * For example, if you've got the list , sort it as  to see that several pairs have the minimum difference of : . The return array would be .
+ *
+ * Given a list of unsorted integers, , find the pair of elements that have the smallest absolute difference between them. If there are multiple pairs, find them all.
+ *
+ * Function Description
+ *
+ * Complete the closestNumbers function in the editor below. It must return an array of integers as described.
+ *
+ * closestNumbers has the following parameter(s):
+ *
+ * arr: an array of integers
+ * Input Format
+ *
+ * The first line contains a single integer , the length of .
+ * The second line contains  space-separated integers, .
+ *
+ * Constraints
+ *
+ * All  are unique in .
+ * Output Format
+ *
+ * Output the pairs of elements with the smallest difference. If there are multiple pairs, output all of them in ascending order, all on the same line with just a single space between each pair of numbers. A number may be part of two pairs when paired with its predecessor and its successor.
+ *
+ * Sample Input 0
+ *
+ * 10
+ * -20 -3916237 -357920 -3620601 7374819 -7330761 30 6246457 -6461594 266854 
+ *  Sample Output 0
+ *
+ *  -20 30
+ *  Explanation 0
+ *  (30) - (-20) = 50, which is the smallest difference.
+ *
+ *  Sample Input 1
+ *
+ *  12
+ *  -20 -3916237 -357920 -3620601 7374819 -7330761 30 6246457 -6461594 266854 -520 -470 
+ *  Sample Output 1
+ *
+ *  -520 -470 -20 30
+ *  Explanation 1
+ *  (-470) - (-520) = 30 - (-20) = 50, which is the smallest difference.
+ *
+ *  Sample Input 2
+ *
+ *  4
+ *  5 4 3 2
+ *  Sample Output 2
+ *
+ *  2 3 3 4 4 5
+ *  Explanation 2
+ *  Here, the minimum difference will be 1. Valid pairs are (2, 3), (3, 4), and (4, 5). We print the elements of each pair, space-separated on a single line.
+ */
+#include <bits/stdc++.h>
+#include<climits>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the closestNumbers function below.
+vector<int> closestNumbers(vector<int> arr) {
+   vector<pair<int,int> > result;
+    vector<int> output;
+    int max_difference = INT_MAX;
+    sort(arr.begin(),arr.end());
+    for(int i=0; i<arr.size();i++){
+	    if(abs(arr[i]-arr[i+1])<max_difference){
+		    max_difference = abs(arr[i]-arr[i+1]);
+		    result.clear();
+		    result.push_back(make_pair(arr[i],arr[i+1]));
+	    }else if(abs(arr[i]-arr[i+1])==max_difference){
+		    result.push_back(make_pair(arr[i],arr[i+1]));
+	    }
+    }
+    for(int i=0;i<result.size();i++){
+	  //  cout<<result[i].first<<" "<<result[i].second<<endl;
+	  output.push_back(result[i].first);
+	  output.push_back(result[i].second);
+    }
+    return output;
+}
+
+int main()
+{
+    ofstream fout("closestsumoutput.txt");
+    ifstream fin("closestsuminput1.txt");
+    if(!fin.is_open()){
+	    cout<<"Input file cann't be open exited"<<endl;
+	    exit(1);
+    }
+
+    int n;
+    //cin >> n;
+    fin>>n;
+   // cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    fin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    //getline(cin, arr_temp_temp);
+    getline(fin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = closestNumbers(arr);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
